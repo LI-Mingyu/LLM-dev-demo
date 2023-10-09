17615da62613c9155f8d84afb4b520b7849a132a
LI-Mingyu <limingyu@ict.ac.cn>
2023-10-09 22:59:17 +0800

    调整了抓取策略
    - 缩小抓取规模，限定到ecs相关的内容，并且主要是troubleshooting部分；
    - 经过对阿里云文档页面的分析，发现除去菜单和推荐链接，主要内容都在[@id="aliyun-docs-view"]标签内；
    - 取消了深度检查，因为还是希望尽可能全面地获取知识库的内容，鉴于scrapy本身已经防止重复抓取了，并且我们先顶了只读取和解析[@id="aliyun-docs-view"]内的内容，取消深度检查没问题。

--- scrapy-prj-aliyunecs/aliyunecs/spiders/qa.py
+++ scrapy-prj-aliyunecs/aliyunecs/spiders/qa.py
@@ -3,22 +3,19 @@ import scrapy
 class QaSpider(scrapy.Spider):
     name = "qa"
     allowed_domains = ["help.aliyun.com"]
-    start_urls = ["https://help.aliyun.com/zh/ecs/support/faq-3"]
-    custom_settings = {
-        'DEPTH_LIMIT': 3,
-        'DUPEFILTER_CLASS': 'scrapy.dupefilters.RFPDupeFilter'
-    }
+    start_urls = ["https://help.aliyun.com/zh/ecs/support/troubleshooting-1/"]
 
     def parse(self, response):
-        # 检查响应的深度
-        depth = response.meta.get('depth', 0)
-        if depth < 3:
-            # 提取页面中的链接
-            for href in response.css('a::attr(href)').extract():
-                yield response.follow(href, self.parse)
-        
-        # 提取页面的内容
-        yield {
-            'url': response.url,
-            'content': response.css('body').get(),
-        }
+        # 提取<section class="aliyun-docs-view" id="aliyun-docs-view">标签内的内容
+        section_content = response.xpath('//*[@id="aliyun-docs-view"]').get()
+        if section_content:
+            # 提取该标签内的链接并跟踪
+            for href in response.xpath('//*[@id="aliyun-docs-view"]//a/@href').extract():
+                if href.startswith('/zh/ecs/support/'):
+                    yield response.follow(href, self.parse)
+            
+            # 提取页面的内容
+            yield {
+                'url': response.url,
+                'content': section_content,
+            }
