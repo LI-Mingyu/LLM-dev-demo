c24c052da1bf9d0e0b9b80c02e5ecec6045b1def
LI-Mingyu <limingyu@ict.ac.cn>
2025-05-12 10:55:11 +0800

    支持 Qwen 3 思维链开关

--- first_llm_call.py
+++ first_llm_call.py
@@ -5,13 +5,15 @@ from openai import OpenAI
 client = OpenAI(api_key="sk-lyczdgelbgsbknxvlbqaxpeodwkpzaxsnyqdilfnfizkbtif", base_url="https://api.siliconflow.cn/v1/")
 
 response = client.chat.completions.create(
-    model="deepseek-ai/DeepSeek-V3",
+    # model="deepseek-ai/DeepSeek-V3",
+    model="Qwen/Qwen3-235B-A22B",
     messages=[
         # {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "system", "content": "You are a helpful assistant"},
+        # {"role": "system", "content": "/no_think"},
         {"role": "user", "content": "Hello!"},
 
     ],
+    extra_body={"enable_thinking": True, "thinking_budget": 32768},
     stream=False
 )
 
